8d10552bac2062449e56829974f2cc5f5de045fe
jamiebull1 <^tA2nq%K&F8TB&At>
2019-10-11 01:34:10 +0200

    Convert various classes to functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,112 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Flexbox from "flexbox-react";
+import { MTLModel } from "react-3d-viewer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: "",
+      imgs: [
+        "1ZoneDataCenterCRAC_wApproachTemp",
+        "1ZoneDataCenterCRAC_wPumpedDXCoolingCoil",
+        "1ZoneEvapCooler",
+        "1ZoneUncontrolled",
+        "1ZoneUncontrolled3SurfaceZone"
+      ],
+      viewerImg: "1ZoneDataCenterCRAC_wApproachTemp"
+    };
+  }
+  setViewerImg(title) {
+    this.setState({ viewerImg: title });
+  }
+  handleChange = event => {
+    this.setState({ searchValue: event.target.value });
+  };
+  renderThumb(img, idx) {
+    return (
+      <li className="thumb-nail">
+        <img
+          key={idx}
+          title={img}
+          alt={img}
+          src={require(`./assets/${img}/idf.png`)}
+          onClick={() => this.setViewerImg(img)}
+        />
+        <p className="thumb-text">{img}</p>
+      </li>
+    );
+  }
+  render() {
+    const thumbs = this.state.imgs.map((img, idx) => {
+      return contains(img, this.state.searchValue)
+        ? this.renderThumb(img, idx)
+        : "";
+    });
+    return (
+      <Flexbox flexDirection="row" minWidth="99vw">
+        <IDFPicker
+          searchValue={this.state.searchValue}
+          thumbs={thumbs}
+          handleChange={this.handleChange}
+        />
+        <IDFViewer img={this.state.viewerImg} />
+      </Flexbox>
+    );
+  }
+}
+
+function contains(str, substr) {
+  return !substr || str.toLowerCase().includes(substr.toLowerCase());
+}
+
+function IDFPicker({ searchValue, handleChange, thumbs }) {
+  return (
+    <div id="picker">
+      <h2>IDF Picker</h2>
+      <input type="text" value={searchValue} onChange={handleChange} />
+      <div id="pickerScrollbox">
+        <ul>{thumbs}</ul>
+      </div>
+    </div>
+  );
+}
+
+function IDFViewer({ img }) {
+  return (
+    <Flexbox flexDirection="column" minHeight="97vh">
+      <IDFImage img={img} />
+      <IDFData img={img} />
+    </Flexbox>
+  );
+}
+function IDFImage({ img }) {
+  return (
+    <div>
+      <h2>{img} model</h2>
+      <MTLModel
+        key={img}
+        src={require(`./assets/${img}/idf.obj`)}
+        mtl={require(`./assets/${img}/idf.mtl`)}
+        width={600}
+        height={400}
+        rotation={{ x: -45, y: 0, z: 0 }}
+        position={{ x: 0, y: 0, z: 0 }}
+      />
+    </div>
+  );
+}
+
+function IDFData({ img }) {
+  return (
+    <div>
+      <h2>{img} IDF data</h2>
+      <p>Lorem ipsem...</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
